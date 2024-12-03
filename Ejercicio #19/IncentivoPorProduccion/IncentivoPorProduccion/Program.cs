84b6d7b145668f9d05fe6935852dbffb55275ffc
Justin Omar Ortiz Diaz <161848522+justininfo@users.noreply.github.com>
2024-12-03 10:32:31 -0400

    Add files via upload

---
+++ Ejercicio #19/IncentivoPorProduccion/IncentivoPorProduccion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lunes, martes, miercoles, jueves, viernes;
+        Console.WriteLine("Ingrese la producción del lunes:");
+        lunes = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la producción del martes:");
+        martes = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la producción del miércoles:");
+        miercoles = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la producción del jueves:");
+        jueves = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la producción del viernes:");
+        viernes = Convert.ToInt32(Console.ReadLine());
+
+        int totalproduccion = lunes + martes + miercoles + jueves + viernes;
+        double promedioproduccion = totalproduccion / 5;
+
+        Console.WriteLine("Total de la producción semanal: " + totalproduccion);
+        Console.WriteLine("Promedio de la producción semanal: " + promedioproduccion);
+
+        if (promedioproduccion >= 20000)
+        {
+            Console.WriteLine("Incentivo: 3000");
+        }
+        else
+        {
+            Console.WriteLine("Incentivo: N/A");
+        }
+    }
+}
---
+++ Ejercicio #20/incentivoPorHijos/incentivoPorHijos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Schema;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el sueldo del empleado:");
+        double sueldo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número de hijos del empleado:");
+        int numeroHijos = Convert.ToInt32(Console.ReadLine());
+
+
+        if (numeroHijos > 0)
+        {
+            double incentivo = numeroHijos * 500;
+            double total = sueldo + incentivo;
+
+            Console.WriteLine("El sueldo del empleado es:" + sueldo);
+            Console.WriteLine("Hijos del empleado:" + numeroHijos);
+            Console.WriteLine("El empleado tuvo un incentivo de:" + incentivo);
+            Console.WriteLine("Sueldo Total del empleado es:" +  total);
+        }
+        else
+        {
+            Console.WriteLine("No le corrresponde, lo siento.");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+
+
+    }
+}
