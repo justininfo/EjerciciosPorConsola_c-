1c9e4dde85ef2c39b17282b4bbeaeaed938ea751
Justin Omar Ortiz Diaz <161848522+justininfo@users.noreply.github.com>
2024-12-03 10:22:40 -0400

    Add files via upload

---
+++ Ejercicios #8/entrarDiscoteca #8/entrarDiscoteca/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double edad ;
+        string genero;
+       
+
+        Console.Write("Ingrese su edad: ");
+        edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.Write("Ingrese su genero: ");
+            genero = Convert.ToString(Console.ReadLine());
+
+
+            if (genero == "Hembra" || genero == "hembra")
+            {
+                Console.Write("Puedes pasar gratis ");
+            }
+
+            else if (genero == "Hombre" || genero == "hombre")
+            {
+                Console.Write("Eres hombre, debes pagar para pasar");
+            }
+        }
+
+        else
+        {
+            Console.Write("Eres menor de edad, no puedes pasar");
+        }
+
+
+    }
+}
