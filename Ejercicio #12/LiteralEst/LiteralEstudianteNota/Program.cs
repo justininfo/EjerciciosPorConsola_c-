89a139eaf3a66e09d5f3153798724ab00eb4ce49
Justin Omar Ortiz Diaz <161848522+justininfo@users.noreply.github.com>
2024-12-03 10:27:23 -0400

    Add files via upload

---
+++ Ejercicio #12/LiteralEst/LiteralEstudianteNota/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double calificación;
+
+
+        Console.Write("Ingrese la calificación del estudiante: ");
+        calificación = Convert.ToDouble(Console.ReadLine());
+
+
+        if (calificación > 100)
+        {
+            Console.WriteLine("La calificación no puede ser mayor a 100:");
+        }
+
+        else if (calificación >= 90)
+        {
+            Console.WriteLine("La calificacion del estudiante es:" + calificación);
+
+            Console.WriteLine("El estudiante tuvo una calificacion excelente");
+        }
+
+        else if (calificación >= 80)
+        {
+            Console.WriteLine("La calificacion del estudiante es:" + calificación);
+
+            Console.WriteLine("El estudiante tuvo una calificacion muy buena");
+        }
+
+        else if (calificación >= 75)
+        {
+            Console.WriteLine("La calificacion del estudiante es:" + calificación);
+
+            Console.WriteLine("El estudiante tuvo una calificacion buena");
+        }
+
+        else if (calificación >= 70)
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta regular!");
+        }
+
+        else 
+        {
+            Console.WriteLine("La calificación ingresada fue: " + calificación);
+
+            Console.WriteLine("¡La situación del estudiante esta deficiente!");
+        }
+
+        
+
+
+        
+
+
+
+    }
+}
---
+++ Ejercicio #13/numPositivoNegativo/numPositvoNegativo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        int num;
+
+
+        Console.Write("Ingrese un numero entero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        
+
+        if (num >= 0)
+        {
+            Console.WriteLine("¡El numero ingresado es: " + num);
+            Console.WriteLine("¡El numero es positivo!");
+        }
+
+        else
+        {
+            Console.WriteLine("¡El numero ingresado es negativo!");
+        }
+
+
+
+
+
+      
+    }
+}
