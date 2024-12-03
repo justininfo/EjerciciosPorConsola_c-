b234263714a4452ee8a3141820415cb122774342
Justin Omar Ortiz Diaz <161848522+justininfo@users.noreply.github.com>
2024-12-03 10:29:14 -0400

    Add files via upload

---
+++ Ejercicio #14/numPar_Impar/numPar_Impar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        int num;
+        
+
+        Console.Write("Ingrese un numero entero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("¡El numero ingresado es: " + num);
+            Console.WriteLine("¡El numero es par!");
+        }
+
+        else
+        {
+            Console.WriteLine("¡El numero ingresado es: " + num);
+            Console.WriteLine("¡El numero ingresado es impar!");
+        }
+
+
+
+
+
+      
+    }
+}
