8be4afc426f289ae3c24cc5069bfed98ffd009ba
Justin Omar Ortiz Diaz <161848522+justininfo@users.noreply.github.com>
2024-12-03 10:30:43 -0400

    Add files via upload

---
+++ Ejercicio #15/numMayorMenor/numMayorMenor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        double num1;
+        double num2;
+        
+
+        Console.Write("Ingrese un primer número entero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese un segundo número entero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("El número mayor es: " + num1);
+        }
+
+        else if (num1 < num2)
+        {
+            Console.WriteLine("El número mayor es: " + num2);
+        }
+
+        else
+        {
+
+            Console.Write("¡Ambos números son iguales!");
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Ejercicio #16/numMayorMenor3num/numMayorMenor3num/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        double num1;
+        double num2;
+        double num3;
+
+
+        Console.Write("Ingrese un primer número entero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese un segundo número entero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese un tercer número entero: ");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+
+
+        if (num1 > num2 && num1 > num3)
+        {
+            Console.WriteLine("El número mayor es: " + num1);
+        }
+
+        else if (num1 < num2 && num2 > num3)
+        {
+            Console.WriteLine("El número mayor es: " + num2);
+        }
+
+        else 
+        {
+            Console.WriteLine("El número mayor es: " + num3);
+        }
+
+        
+
+
+
+
+
+
+
+
+    }
+}
